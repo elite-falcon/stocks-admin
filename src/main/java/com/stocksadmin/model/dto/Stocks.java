13c9a050c2adb203432cbf42ac81696eaab1018b
Arthur Silva <fuscellaarthur@gmail.com>
2020-05-24 13:38:26 -0300

    added stocks model and transformation

--- src/main/java/com/stocksadmin/model/dto/Stocks.java
+++ src/main/java/com/stocksadmin/model/dto/Stocks.java
@@ -1,4 +1,61 @@
 package com.stocksadmin.model.dto;
 
-public class Stocks {
+import java.util.UUID;
+
+public class Stocks implements StocksDTO {
+
+	private UUID uuid;
+	private String code;
+	private long createdAt;
+	private long modifiedAt;
+	private boolean active;
+
+	public Stocks() {
+
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public long getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(long createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public long getModifiedAt() {
+		return modifiedAt;
+	}
+
+	public void setModifiedAt(long modifiedAt) {
+		this.modifiedAt = modifiedAt;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return new StringBuilder().append(this.getUuid()).append(this.getCode()).toString();
+	}
 }
