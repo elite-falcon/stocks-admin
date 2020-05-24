76f584bf957b2ccd1274b34f4e83b671bc23f6c8
Arthur Silva <fuscellaarthur@gmail.com>
2020-05-24 13:40:14 -0300

    added persistence repository

--- src/main/java/com/stocksadmin/persistence/repository/StocksConfigurationRepository.java
+++ src/main/java/com/stocksadmin/persistence/repository/StocksConfigurationRepository.java
@@ -1,9 +1,30 @@
 package com.stocksadmin.persistence.repository;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.stocksadmin.model.entity.StocksConfiguration;
 
+@Repository
 public interface StocksConfigurationRepository extends JpaRepository<StocksConfiguration, String> {
+
+	@Query("SELECT c FROM stocksconfiguration c WHERE c.active = true")
+	List<StocksConfiguration> findActiveStocks();
+
+	@Query("SELECT c FROM stocksconfiguration c WHERE c.uuid = uuid")
+	StocksConfiguration findStocks(@Param("uuid") UUID uuid);
+
+	@Query("SELECT EXISTS(SELECT 1 FROM stocksconfiguration c WHERE c.id = :uuid)")
+	boolean existsByUUID(@Param("uuid") UUID uuid);
+
+	StocksConfiguration save(@Param("StocksConfiguration") StocksConfiguration stocksConfiguration);
+
+	void deleteByUUID(@Param("uuid") UUID uuid) throws EmptyResultDataAccessException;
 
 }
