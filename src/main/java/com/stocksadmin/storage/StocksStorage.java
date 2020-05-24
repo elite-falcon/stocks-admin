e98d8fa493da30b2be79144c9e95634e7b7d7d32
Arthur Silva <fuscellaarthur@gmail.com>
2020-05-24 13:41:51 -0300

    added stocks storage

--- src/main/java/com/stocksadmin/storage/StocksStorage.java
+++ src/main/java/com/stocksadmin/storage/StocksStorage.java
@@ -1,12 +1,18 @@
 package com.stocksadmin.storage;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import javax.transaction.Transactional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
+import com.stocksadmin.exception.AlreadyExistsException;
+import com.stocksadmin.exception.CreateException;
 import com.stocksadmin.exception.NoDataException;
 import com.stocksadmin.exception.StocksAdminException;
 import com.stocksadmin.model.dto.Stocks;
@@ -15,9 +21,9 @@ import com.stocksadmin.model.entity.StocksConfiguration;
 import com.stocksadmin.persistence.repository.StocksConfigurationRepository;
 
 @Component
-public class StocksConfigurationStorage {
+public class StocksStorage {
 
-	private static Logger logger = LogManager.getLogger(StocksConfigurationStorage.class);
+	private static Logger logger = LogManager.getLogger(StocksStorage.class);
 
 	@Autowired
 	private StocksConfigurationRepository stocksConfigurationRepository;
@@ -29,20 +35,86 @@ public class StocksConfigurationStorage {
 	 * @return
 	 * @throws NoDataException
 	 */
-	public List<Stocks> findAll() throws NoDataException {
+	public List<Stocks> findAllStocks() throws StocksAdminException {
 		return treatResultSet(stocksConfigurationRepository.findAll());
 	}
 
-	public List<Stocks> findAllActiveStocks() throws NoDataException {
+	/**
+	 * @return
+	 * @throws StocksAdminException
+	 */
+	public List<Stocks> findAllActiveStocks() throws StocksAdminException {
 		return treatResultSet(stocksConfigurationRepository.findActiveStocks());
 	}
 
+	/**
+	 * @param uuid
+	 * @return
+	 * @throws NoDataException
+	 */
+	public Stocks findStocks(UUID uuid) throws NoDataException {
+		if (stocksConfigurationRepository.existsByUUID(uuid)) {
+			throw new NoDataException(StocksAdminException.NO_DATA);
+		}
+		StocksConfiguration stocksConfiguration = stocksConfigurationRepository.findStocks(uuid);
+		return stocksTransformation.transformToStocks(stocksConfiguration);
+	}
+
+	/**
+	 * @param stocks
+	 * @return
+	 * @throws StocksAdminException
+	 */
+	public Stocks create(Stocks stocks) throws StocksAdminException {
+		if (stocksConfigurationRepository.existsByUUID(stocks.getUuid())) {
+			throw new AlreadyExistsException(StocksAdminException.ALREADY_EXISTS);
+		}
+		return treatSave(stocksTransformation.transformToStocksConfiguration(stocks));
+	}
+
+	/**
+	 * @param uuid
+	 * @throws StocksAdminException
+	 */
+	@Transactional
+	public void deleteStockByUUID(UUID uuid) throws StocksAdminException {
+		try {
+			stocksConfigurationRepository.deleteByUUID(uuid);
+			logger.info("Stocks configuration {} delete successfully", uuid);
+		}
+		catch (EmptyResultDataAccessException e) {
+			logger.error("Could not delete stocks configuration {}", uuid);
+			throw new StocksAdminException(StocksAdminException.DELETION);
+		}
+	}
+
+	/**
+	 * @param stocksConfigurationList
+	 * @return
+	 * @throws NoDataException
+	 */
 	private List<Stocks> treatResultSet(List<StocksConfiguration> stocksConfigurationList) throws NoDataException {
 		if (stocksConfigurationList.isEmpty()) {
 			throw new NoDataException(StocksAdminException.NO_DATA);
 		}
-		logger.info("retrieved {} stocks", stocksConfigurationList.size());
-		return stocksTransformation.transformToStocks(stocksConfigurationList);
+		logger.info("Retrieved {} stocks", stocksConfigurationList.size());
+		return stocksTransformation.transformToStocksList(stocksConfigurationList);
+	}
+
+	/**
+	 * @param stocksConfiguration
+	 * @return
+	 * @throws CreateException
+	 */
+	private Stocks treatSave(StocksConfiguration stocksConfiguration) throws CreateException {
+		StocksConfiguration storedStocksConfiguration = stocksConfigurationRepository.save(stocksConfiguration);
+		if (Objects.isNull(storedStocksConfiguration)) {
+			logger.error("Could not create stocks configuration {}", stocksConfiguration.getCode());
+			throw new CreateException(StocksAdminException.CREATION);
+		}
+		Stocks stocks = stocksTransformation.transformToStocks(storedStocksConfiguration);
+		logger.info("Created stocks configuration: {}", stocks.toString());
+		return stocks;
 	}
 
 }
